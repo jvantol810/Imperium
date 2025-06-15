28f5b331a4be5745f314ac8422c5e481afa103a0
jvantol810 <63761346+jvantol810@users.noreply.github.com>
2025-06-15 16:34:37 -0400

    Added list for testing/development and rerouting based on local profile

--- src/main/java/Imperium/ListMaster/Application/controllers/pages/MainController.java
+++ src/main/java/Imperium/ListMaster/Application/controllers/pages/MainController.java
@@ -2,6 +2,7 @@ package Imperium.ListMaster.Application.controllers.pages;
 
 import Imperium.ListMaster.Application.data.ToDoListItem;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,8 +16,16 @@ import java.util.stream.Collectors;
 @Controller
 public class MainController {
 
+    @Value("${spring.profiles.active:default}")
+    private String activeProfile;
+
     @GetMapping("/home")
     public void getHomePage(HttpServletResponse response) throws IOException {
-        response.sendRedirect("/list-page?name=todolist");
+        if(activeProfile.equals("local")) {
+            response.sendRedirect("/list-page?name=TESTtodolist");
+        }
+        else {
+            response.sendRedirect("/list-page?name=todolist");
+        }
     }
 }
